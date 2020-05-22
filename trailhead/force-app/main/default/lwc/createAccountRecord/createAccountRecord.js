9edced7ded01dd6dd8395ffec2ce86bb6c470d36
ravi0725 <ravisingh0725@gmai.com>
2020-05-22 18:32:04 +0530

    changes

--- trailhead/force-app/main/default/lwc/createAccountRecord/createAccountRecord.js
+++ trailhead/force-app/main/default/lwc/createAccountRecord/createAccountRecord.js
@@ -6,20 +6,28 @@
 @ export : export default keywords export a MyComponent class for other components to use.
 */
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 import accountObject from '@salesforce/schema/Account';
 import nameField from '@salesforce/schema/Account.Name';
 import websiteField from '@salesforce/schema/Account.Website';
 import industryField from '@salesforce/schema/Account.Industry';
+import ACCOUNT_COMPANY from '@salesforce/schema/Account.Account_Company__c';
 
 export default class CreateAccountRecord extends LightningElement {
      // Code for  create the Account Record
      accountObject = accountObject;
-     myFields = [nameField, websiteField, industryField];
+     myFields = [nameField, websiteField, industryField,ACCOUNT_COMPANY];
  
      handleSubmit(){
-         
+          
      }
      handleSuccess() {
-        
+          const evt = new ShowToastEvent({
+               title: "Success!",
+               message: "The Accont's record has been successfully saved.",
+               variant: "success",
+           });
+           this.dispatchEvent(evt);
      }
 }
